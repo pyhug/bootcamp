ecc5073e39e0635a430dec834a49f58cb7ca6193
Yung-Yu Chen <yyc@solvcon.net>
2016-12-08 21:20:46 +0800

    Reduce Vagrant provision time.  On my MBA it was brought down to 13 minutes.

--- vagrant/bootstrap-anaconda.sh
+++ vagrant/bootstrap-anaconda.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-anaconda=Anaconda3-2.4.0-Linux-x86_64.sh
+anaconda=Miniconda3-latest-Linux-x86_64.sh
 
 cd /vagrant
 if [[ ! -f $anaconda ]]; then
-  wget --quiet http://repo.continuum.io/archive/$anaconda
+  wget --quiet http://repo.continuum.io/miniconda/$anaconda
 fi
 
-if [[ `which conda` != "${HOME}/opt/anaconda/bin/conda" ]]; then
-  chmod +x $anaconda
-  ./$anaconda -b -p ${HOME}/opt/anaconda
-  cat >> /home/vagrant/.bashrc << END
+if [[ `which conda` != "${HOME}/opt/conda3/bin/conda" ]]; then
+  bash ./$anaconda -b -p ${HOME}/opt/conda3
+  cat >> $HOME/.bashrc << END
 # add for anaconda install
-PATH=/opt/anaconda/bin:\$PATH
+PATH=\$HOME/opt/conda3/bin:\$PATH
 END
 fi
+
+# vim: set et nobomb fenc=utf8 ft=sh ff=unix sw=2 ts=2:
--- vagrant/setup-dotfiles.sh
+++ vagrant/setup-dotfiles.sh
@@ -2,6 +2,9 @@
 SRCDIR=/vagrant/dotfiles
 DSTDIR=${HOME}
 
+mkdir ${DSTDIR}/self/etc -p
 cp ${SRCDIR}/dot_bashrc ${DSTDIR}/.bashrc
+cp ${SRCDIR}/dot_self_bashrc ${DSTDIR}/self/etc/dot_bashrc
 cp ${SRCDIR}/dot_git-completion.bash ${DSTDIR}/.git-completion.bash
 cp ${SRCDIR}/dot_git-prompt.bash ${DSTDIR}/.git-prompt.bash
+cp ${SRCDIR}/dot_gvimrc ${DSTDIR}/self/etc/dot_gvimrc
